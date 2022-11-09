5ca191fc295bc49d0313a19676260917fe8017a3
tetetratra <44367208+tetetratra@users.noreply.github.com>
2022-11-09 20:13:50 +0900

    add test.rb tailcall.rb

---
+++ tailcall.rb
@@ -0,0 +1,7 @@
+def b(n)
+  if n == 0
+    c()
+  else
+    b(n-1)
+  end
+end
---
+++ test.rb
@@ -0,0 +1,15 @@
+RubyVM::InstructionSequence.compile_option = { tailcall_optimization: true }
+
+def a()
+  b(10)
+end
+
+RubyVM::InstructionSequence.compile_file(
+  File.expand_path('tailcall.rb', __dir__)
+).eval
+
+def c()
+  caller_locations(0)
+end
+
+puts a()
