3c9c952a48a13c92cee60457a5fcdb466ac45533
tetetratra <44367208+tetetratra@users.noreply.github.com>
2023-04-22 10:38:47 +0900

    微修正

--- in_self.rb
+++ in_self.rb
@@ -1,39 +1,42 @@
 $with_self_methods = []
 
-TracePoint.trace(:call) do |tp|
-  next unless $with_self_methods.any?(tp.method_id)
-
-  tracepoint_call
-  $fire_b_call_hook = true
-end
-
-TracePoint.trace(:return) do |tp|
-  next unless $with_self_methods.any?(tp.method_id)
-
-  $fire_b_call_hook = false
-end
-
-TracePoint.trace(:b_call) do |tp|
-  tracepoint_b_call if $fire_b_call_hook
-end
-
-
 class WithProxy
-  def initialize(slf)
-    @slf = slf
+  def initialize(elf)
+    @elf = elf
   end
 
-  def method_missing(name, *args, &block)
-    with_self_method = :"withself_#{name}"
+  def method_missing(method, *args, &block)
+    with_self_method = :"withself_#{method}"
     $with_self_methods << with_self_method
 
     eval <<-EOS
-      def @slf.#{with_self_method}(&b)
-        #{name} { |a| b.(a) }
+      def @elf.#{with_self_method}(*a, &b)
+        #{method}(*a) { |e| b.(e) }
       end
     EOS
 
-    @slf.send(with_self_method, *args, &block)
+    ret = @elf.send(with_self_method, *args, &block)
+    $with_self_methods.pop
+    ret
+  end
+
+  TP_CALL = TracePoint.trace(:call) do |tp|
+    next unless $with_self_methods.any?(tp.method_id)
+
+    tracepoint_call
+    $fire_b_call_hook = true
+  end
+
+  TP_RETURN = TracePoint.trace(:return) do |tp|
+    next unless $with_self_methods.any?(tp.method_id)
+
+    $fire_b_call_hook = false
+  end
+
+  TP_B_CALL = TracePoint.trace(:b_call) do |tp|
+    next unless $fire_b_call_hook
+
+    tracepoint_b_call
   end
 end
 
